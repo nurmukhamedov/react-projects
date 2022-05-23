ba3669edfcdfe1bbaab57b05eb937b5f134d61ba
nurmukhamedov <nurmuxamedovabdurasul@gmail.com>
2022-05-23 05:39:15 +0500

    starter file

---
+++ react-project/src/App.js
@@ -0,0 +1,11 @@
+import Main from "./components/tours/Main";
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-project/src/components/tours/Main.js
@@ -0,0 +1,12 @@
+import React from 'react' 
+import './Main.css'
+
+const Main = () => {
+  return (
+   <main>
+       hello
+   </main>
+  )
+}
+
+export default Main
