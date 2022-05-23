9f13687bbd718c10ee64f1bba572f3625c9ce557
nurmukhamedov <nurmuxamedovabdurasul@gmail.com>
2022-05-23 06:22:34 +0500

    lesson-19

--- react-project/src/components/tours/Tour.js
+++ react-project/src/components/tours/Tour.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const Tour = () => {
+const Tour = ({ id, image, info, name, price, removeTour }) => {
+  const [readMore, setReadMore] = useState(false);
   return (
-    <div>Tour</div>
-  )
-}
+    <article className="single-tour">
+      <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price"> ${price} </h4>
+        </div>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? "show less" : "read more"}
+          </button>
+        </p>
+        <button className="delete-btn" onClick={() => removeTour(id)}>
+          not interested
+        </button>
+      </footer>
+    </article>
+  );
+};
 
-export default Tour
+export default Tour;
