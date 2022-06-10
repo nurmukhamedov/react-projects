d7c41d54f3324cad3fa64e1a4774abee2454e9fe
nurmukhamedov <nurmuxamedovabdurasul@gmail.com>
2022-06-10 10:45:12 +0500

    lesson-30

--- react-project/src/components/quiz/Main.js
+++ react-project/src/components/quiz/Main.js
@@ -1,33 +1,67 @@
 import React from 'react'
-import { useGlobalContext } from './context';
-import SetupForm from './SetupForm';
-import Loading from './Loading';
-import Modal from './Modal';
+import { useGlobalContext } from './context'
 
-import './Main.css';
+import SetupForm from './SetupForm'
+import Loading from './Loading'
+import Modal from './Modal' 
 
-const Main = () => { 
-    
-    const {
-        waiting, 
-        loading, 
-        questions, 
-        index, 
-        correct, 
-        error, 
-        isModalOpen, 
-        quiz
-    } = useGlobalContext();
-    
-    if(waiting) {
-        return <SetupForm/>
-    }
-    if(loading) {
-        return <Loading/>
-    }
+import './Main.css'
+
+function App() {
+  const {
+    waiting,
+    loading,
+    questions,
+    index,
+    correct,
+    nextQuestion,
+    checkAnswer,
+  } = useGlobalContext()
+  if (waiting) {
+    return <SetupForm />
+  }
+  if (loading) {
+    return <Loading />
+  }
+
+  const { question, incorrect_answers, correct_answer } = questions[index]
+  // const answers = [...incorrect_answers, correct_answer]
+  let answers = [...incorrect_answers]
+  const tempIndex = Math.floor(Math.random() * 4)
+  if (tempIndex === 3) {
+    answers.push(correct_answer)
+  } else {
+    answers.push(answers[tempIndex])
+    answers[tempIndex] = correct_answer
+  }
   return (
-    <div>Main</div>
+    <main>
+      <Modal />
+      <section className='quiz'>
+        <p className='correct-answers'>
+          correct answers : {correct}/{index}
+        </p>
+        <article className='container'>
+          <h2 dangerouslySetInnerHTML={{ __html: question }} />
+          <div className='btn-container'>
+            {answers.map((answer, index) => {
+              return (
+                <button
+                  key={index}
+                  className='answer-btn'
+                  onClick={() => checkAnswer(correct_answer === answer)}
+                  dangerouslySetInnerHTML={{ __html: answer }}
+                />
+              )
+            })}
+          </div>
+        </article>
+        <button className='next-question' onClick={nextQuestion}>
+          next question
+        </button>
+      </section>
+    </main>
   )
 }
 
-export default Main;
+export default App
--- react-project/src/components/quiz/context.js
+++ react-project/src/components/quiz/context.js
@@ -1,12 +1,11 @@
-import axios from 'axios';
-import React, { useState, useContext, useEffect } from 'react' 
+import axios from 'axios'
+import React, { useState, useContext, useEffect } from 'react'
 
 const table = {
-    sports: 21, 
-    history: 23, 
-    politics: 24
+  sports: 21,
+  history: 23,
+  politics: 24,
 }
-
 
 const API_ENDPOINT = 'https://opentdb.com/api.php?'
 
@@ -15,76 +14,97 @@ const tempUrl =
   'https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple'
 const AppContext = React.createContext()
 
-const AppProvider = ({ children }) => { 
-    const [waiting, setWaiting] = useState(true)
-    const [loading, setLoading] = useState(false)
-    const [questions, setQuestions]= useState([])
-    const [index, setIndex] = useState(0)
-    const [correct, setCorrect] = useState(0)
-    const [error, setError] = useState(false)
-    const [quiz, setQuiz] = useState({
-        amount: 10, 
-        category: 'sports', 
-        difficulty: 'easy',
-    })
-    
-    const [isModalOpen, steIsModalOpen] = useState(false);
-    
-    const fetchQuestions = async (url) => {
-        setLoading (true)
+const AppProvider = ({ children }) => {
+  const [waiting, setWaiting] = useState(true)
+  const [loading, setLoading] = useState(false)
+  const [questions, setQuestions] = useState([])
+  const [index, setIndex] = useState(0)
+  const [correct, setCorrect] = useState(0)
+  const [error, setError] = useState(false)
+  const [quiz, setQuiz] = useState({
+    amount: 10,
+    category: 'sports',
+    difficulty: 'easy',
+  })
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const fetchQuestions = async (url) => {
+    setLoading(true)
+    setWaiting(false)
+    const response = await axios(url).catch((err) => console.log(err))
+    if (response) {
+      const data = response.data.results
+      if (data.length > 0) {
+        setQuestions(data)
+        setLoading(false)
         setWaiting(false)
-        const response = await axios(url).catch((error) => console.log(error));
-        if(response) {
-            const data = response.data.results
-            if (data.length > 0) {
-                setQuestions(data)
-                setLoading(false)
-                setWaiting(false)
-                setError(false)
-            } else {
-                setWaiting(true)
-                setError(true)
-            }
-        } else {
-            setWaiting(true)
-        }
-    }  
-    const nextQuestion = () => {
-        setIndex((oldIndex) => {
-            const index = oldIndex + 1
-            if( index > questions.length - 1) {
-                openModal()
-                return 0
-            } else {
-                return index
-            }
-        })
-    } 
-    const checkAnswer = (value) => {
-        if(value) {
-            setCorrect((oldState) => oldState + 1)
-        }
-        nextQuestion()
+        setError(false)
+      } else {
+        setWaiting(true)
+        setError(true)
+      }
+    } else {
+      setWaiting(true)
     }
-    
-    const openModal = () => {
-        steIsModalOpen(true)
+  }
+
+  const nextQuestion = () => {
+    setIndex((oldIndex) => {
+      const index = oldIndex + 1
+      if (index > questions.length - 1) {
+        openModal()
+        return 0
+      } else {
+        return index
+      }
+    })
+  }
+  const checkAnswer = (value) => {
+    if (value) {
+      setCorrect((oldState) => oldState + 1)
     }
-    
-    
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        
-        const {amount, category, difficulty } = quiz
-        
-        const url = `${API_ENDPOINT}amount=${amount}&diffuclty=${difficulty}&category=${table[category]}&type=multiple`
-        fetchQuestions(url)
-    }
+    nextQuestion()
+  }
+
+  const openModal = () => {
+    setIsModalOpen(true)
+  }
+  const closeModal = () => {
+    setWaiting(true)
+    setCorrect(0)
+    setIsModalOpen(false)
+  }
+  const handleChange = (e) => {
+    const name = e.target.name
+    const value = e.target.value
+    setQuiz({ ...quiz, [name]: value })
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const { amount, category, difficulty } = quiz
+
+    const url = `${API_ENDPOINT}amount=${amount}&difficulty=${difficulty}&category=${table[category]}&type=multiple`
+    fetchQuestions(url)
+  }
 
   return (
-    <AppContext.Provider value={{
-        waiting, loading, questions, index, correct, error, isModalOpen, quiz, nextQuestion, checkAnswer, handleSubmit
-    }}>
+    <AppContext.Provider
+      value={{
+        waiting,
+        loading,
+        questions,
+        index,
+        correct,
+        error,
+        isModalOpen,
+        nextQuestion,
+        checkAnswer,
+        closeModal,
+        quiz,
+        handleChange,
+        handleSubmit,
+      }}
+    >
       {children}
     </AppContext.Provider>
   )
